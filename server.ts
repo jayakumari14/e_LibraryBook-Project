063d87efaaaad74e1b83ac97189d0312a719452b
jayak <kumarijaya127@gmail.com>
2024-04-22 23:00:43 +0530

    added config

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
+import { config } from "./src/config/config";
 import app from "./src/app";
 
 
 const startServer=()=>{
-    const port= process.env.PORT||3000
+    const port= config.port||3000;
     app.listen(port,()=>{
         console.log(`listening on port: ${port}`);
         
