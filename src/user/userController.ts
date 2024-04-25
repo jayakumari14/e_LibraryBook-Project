e4bfdf2252f7536d20122c2041a6bacc4fab75cd
jayak <kumarijaya127@gmail.com>
2024-04-25 15:24:33 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -16,19 +16,19 @@ const createUser=async(req:Request,res:Response,next:NextFunction)=>{
 
 
     // Database call
-try {
-    const user=await userModel.findOne({email})
-    if(user){
+    try {
+      const user=await userModel.findOne({email})
+      if(user){
         const error=createHttpError(400,"User already exists with this email")
         return next(error)
-    }
-} catch (err) {
-    return next(createHttpError(500,"Error while getting user"))
-}
+        }
+       } catch (err) {
+       return next(createHttpError(500,"Error while getting user"))
+       }
       
 
     ///password--> hash
-let newUser:User
+     let newUser:User
 
     const hashedPassword=await bcrypt.hash(password,10);
     try {
@@ -44,21 +44,46 @@ let newUser:User
 
     // Token generation,JWT tokens
 
-try {
+    try {
     const token= sign({sub:newUser._id}, config.jwtSecret as string,{
         expiresIn:'7d',
         algorithm:"HS256",
     })
     // response
-
-   res.status(201).json({accessToken:token});
-  }catch (err) {
+    res.status(201).json({accessToken:token});
+    }catch (err) {
     return next(createHttpError(500,"Error while signing the jwt token"));
-}
+    }
 }   
 
+
+// ///////  LOGIN USER   ////////////////
 const loginUser=async(req:Request,res:Response,next:NextFunction)=>{
-    res.json({message:"OK"})
-}
+    const {email,password}=req.body
+    if(!email||!password){
+        return next(createHttpError(400, "All fields are required"))
+    }
+   
+     // todo: wrap in try catch.
+     
+    const user=await userModel.findOne({email})
+     if(!user){
+        return next(createHttpError(404,"user not found"))
+      }
+
+   const isMatch=await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        return next(createHttpError(400,"UserName or password incorrect"))
+        }
+   
+// create access token
+
+    const token= sign({sub:user._id}, config.jwtSecret as string,{
+        expiresIn:'7d',
+        algorithm:"HS256",
+    })
+    res.json({accessToken:token});
+};
+     
 
 export  {createUser,loginUser}
